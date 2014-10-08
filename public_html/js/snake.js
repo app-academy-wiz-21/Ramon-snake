79f86df1978ad9e2753dccfe9c2664722f0fd4d3
Ramon <ac2262ra0530@pusd.us>
2014-10-08 15:00:47 -0700

    finished two videos had troubles but fixed them

---
+++ public_html/js/snake.js
@@ -0,0 +1,17 @@
+ var snake;
+ 
+ var context;
+ var screenwidth;
+ var screenheight;
+ 
+ function gameInitialize () {
+     
+ } 
+ 
+ function gameLoop () {
+     
+ }
+ 
+ function gameDraw () {
+     
+ }
